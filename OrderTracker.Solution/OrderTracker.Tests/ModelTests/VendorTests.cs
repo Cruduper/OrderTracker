574113cc472c4e1d7c0e1390eaa6604ce82d3c60
Eric Crudup <briefcasemanx@gmail.com>
2022-03-04 11:21:48 -0800

    Red: Vendor Constructor object creation good fail

--- OrderTracker.Solution/OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Solution/OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -5,12 +5,19 @@ using System;
 
 namespace OrderTracker.Tests
 {
+  [TestClass]
   public class VendorTests : IDisposable
   {
     public void Dispose()
     {
     }
 
+    [TestMethod]
+    public void VendorConstructor_CreatesVendorInstance_Vendor()
+    {
+      Vendor newVendor = new Vendor("a name", "a description");
 
+      Assert.AreEqual(typeof(int), newVendor.GetType());
+    }
   }
 }
