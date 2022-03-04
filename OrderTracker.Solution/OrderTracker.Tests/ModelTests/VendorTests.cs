2693bb384dfc7e9464d67c905ea45108b07a538c
Eric Crudup <briefcasemanx@gmail.com>
2022-03-04 12:53:07 -0800

    Red: static _vendors field stores previous Vendors instances good fail

--- OrderTracker.Solution/OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Solution/OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -10,12 +10,13 @@ namespace OrderTracker.Tests
   {
     public void Dispose()
     {
+      //Vendor.ClearAll();
     }
 
     [TestMethod]
     public void VendorConstructor_CreatesVendorInstance_Vendor()
     {
-      Vendor newVendor = new Vendor("a name", "a description");
+      Vendor newVendor = new Vendor("a name0", "a description0");
 
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
@@ -23,23 +24,54 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void VendorConstructor_SetsNamePropertyCorrectly_CorrectNameText()
     {
-      string nam = "a name";
-      string descr = "a description";
+      string nam = "a name1";
+      string descr = "a description1";
 
       Vendor newVendor = new Vendor(nam, descr);
 
       Assert.AreEqual(nam, newVendor.Name);
     }
 
-        [TestMethod]
+    [TestMethod]
     public void VendorConstructor_SetsDescriptionPropertyCorrectly_CorrectDescriptionText()
     {
-      string nam = "a name";
-      string descr = "a description";
+      string nam = "a name2";
+      string descr = "a description2";
 
       Vendor newVendor = new Vendor(nam, descr);
 
       Assert.AreEqual(descr, newVendor.Description);
     }
+
+    [TestMethod]
+    public void VendorConstructor_ChecksThatPreviousTestedInstancesOfVendorsAreStoredInVendorsField_VendorsCountEquals3AndAllPropertiesInAll3InstancesHaveExpectedValues()
+    {
+
+      Assert.AreEqual(4, Vendor.GetAll().Count);
+      Assert.AreEqual("a name0", Vendor.GetAll()[0].Name);
+      Assert.AreEqual("a description0", Vendor.GetAll()[0].Description);
+      Assert.AreEqual(0, Vendor.GetAll()[0].Orders.Count);
+      Assert.AreEqual("a name1", Vendor.GetAll()[1].Name);
+      Assert.AreEqual("a description1", Vendor.GetAll()[1].Description);
+      Assert.AreEqual(0, Vendor.GetAll()[1].Orders.Count);
+      Assert.AreEqual("a name2", Vendor.GetAll()[2].Name);
+      Assert.AreEqual("a description2", Vendor.GetAll()[2].Description);
+      Assert.AreEqual(0, Vendor.GetAll()[2].Orders.Count);
+    }
+
+    // [TestMethod]
+    // public void ClearAll_ClearsPreviousInstancesOf
+
+    // [TestMethod]
+    // public void ClearAll_DestroysAllVendorInstancesFromVendorsField_NoInstances()
+    // {
+    //   Vendor vend1 = new Vendor("name1", "descr1");
+    //   Vendor vend2 = new Vendor("name2", "descr2");
+    //   List<Vendor> vendList = new List<Vendor>{ vend1, vend2};
+    //   //Vendor.ClearAll();
+
+    //   //Assert.AreEqual(1, Vendor.GetAll().Count);
+    //   CollectionAssert.AreEqual(vendList, Vendor.GetAll());
+    // }
   }
 }
