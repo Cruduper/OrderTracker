33bbceb98c6dc5fec1a5ee238e60bee9332b55b0
Eric Crudup <briefcasemanx@gmail.com>
2022-03-04 14:18:25 -0800

    Red: Vendor.GetAll() test good fail

--- OrderTracker.Solution/OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Solution/OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -10,7 +10,7 @@ namespace OrderTracker.Tests
   {
     public void Dispose()
     {
-      //Vendor.ClearAll();
+      Vendor.ClearAll();
     }
 
     [TestMethod]
@@ -43,11 +43,37 @@ namespace OrderTracker.Tests
       Assert.AreEqual(descr, newVendor.Description);
     }
 
+    // ClearAll() and GetAll() as outlined in the weekly lessons have a circular
+    // dependency. You cannot test one without the other, because we do not have public
+    // access to the private field named "_vendors" (I renamed this, it was
+    // "_instances" in the lesson).
+    // For this test I enabled Dispose (which uses ClearAll), without having first tested ClearAll, as per the lesson.
+    // This dependency could be broken by having testing variables scoped outside
+    // of specific tests, so they could be reused, or by making _vendors public
+    // which is probably a bad idea.
     [TestMethod]
+    public void GetAll_ReturnsVendorsField_Vendors()
+    {
+      Vendor newVendor0 = new Vendor("a name0", "a description0");
+      Vendor newVendor1 = new Vendor("a name1", "a description1");
+
+      List<Vendor> vendList = new List<Vendor>{newVendor0};
+
+      CollectionAssert.AreEqual(vendList, Vendor.GetAll());
+    }
+
+    // [TestMethod]
     // public void ClearAll_ClearsAllVendorInstancesInVendorsField_0()
     // {
+    
+    //   Vendor newVendor0 = new Vendor("a name0", "a description0");
+    //   Vendor newVendor1 = new Vendor("a name1", "a description1");
 
+    //   List<Vendor> vendList = new List<Vendor>{newVendor0, newVendor1};
+
+    //   Assert.AreEqual(3. Vendor.GetAll());
     // }
+
     // [TestMethod]
     // public void VendorConstructor_ChecksThatPreviousTestedInstancesOfVendorsAreStoredInVendorsField_VendorsCountEquals3AndAllPropertiesInAll3InstancesHaveExpectedValues()
     // {
