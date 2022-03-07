b8ea1bc5e1c002bf6c6e1c6c2d7671b3b00ed5d1
Eric Crudup <briefcasemanx@gmail.com>
2022-03-06 17:01:12 -0800

    adds Pastry and Bread tests from external PierresBakery project

---
+++ OrderTracker.Solution/OrderTracker.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfItemBread_BreadInstance()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void SetPrice_SetsPriceOfBreadTo5_5()
+    {
+      Bread newBread = new Bread();
+      newBread.Price = 5;
+      Assert.AreEqual(5, newBread.Price);
+    }
+
+    [TestMethod]
+    public void BreadConstructor_SetsPriceOfBreadTo5UsingOverloadedBreadConstructor_5()
+    {
+      Bread newBread = new Bread(5);
+      Assert.AreEqual(5, newBread.Price);
+    }
+
+    [TestMethod]
+    public void BreadDiscounter_ReturnsAPriceForAListofBreadsWithOneElement_5()
+    {
+      List<Bread> loaves = new List<Bread>{ new Bread(5) };
+      Assert.AreEqual(5, loaves[0].Price);
+    }
+
+    [TestMethod]
+    public void BreadDiscounter_ReturnsAPriceForAListofBreadsWithTenElements_35()
+    {
+      List<Bread> loaves = new List<Bread>{ new Bread(5), new Bread(5), new Bread(5), new Bread(5), new Bread(5), new Bread(5), new Bread(5), new Bread(5), new Bread(5), new Bread(5) };
+      Assert.AreEqual(35, Bread.BreadDiscounter(loaves));
+    }
+  }
+}
---
+++ OrderTracker.Solution/OrderTracker.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfItemPastry_PastryInstance()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
+    [TestMethod]
+    public void PastryDotSetPrice_SetsPriceOfPastryTo5_5()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Pastry newPastry = new Pastry();
+      newPastry.Price = 5;
+      Assert.AreEqual(5, newPastry.Price);
+    }
+
+    [TestMethod]
+    public void PastryConstructor_SetsPriceOfPastryTo2UsingOverloadedPastryConstructor_2()
+    {
+      Pastry newPastry = new Pastry(2);
+      Assert.AreEqual(2, newPastry.Price);
+    }
+
+    [TestMethod]
+    public void PastryDiscounter_ReturnsAPriceForAListofPastrysWithOneElement_2()
+    {
+      List<Pastry> pastes = new List<Pastry>{ new Pastry(2) };
+      Assert.AreEqual(2, Pastry.PastryDiscounter(pastes));
+    }
+
+    [TestMethod]
+     public void PastryDiscounter_ReturnsAPriceForAListofPastrysWithThreeElements_5()
+    {
+      List<Pastry> pastes = new List<Pastry>{ new Pastry(2), new Pastry(2), new Pastry(2)};
+      Assert.AreEqual(5, Pastry.PastryDiscounter(pastes));
+    }
+
+    [TestMethod]
+     public void PastryDiscounter_ReturnsAPriceForAListofPastrysWithSixElements_10()
+    {
+      List<Pastry> pastes = new List<Pastry>{ new Pastry(2), new Pastry(2), new Pastry(2), new Pastry(2), new Pastry(2), new Pastry(2)};
+      Assert.AreEqual(10, Pastry.PastryDiscounter(pastes));
+    }
+  }
+}
