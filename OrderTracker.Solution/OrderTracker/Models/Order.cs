bdb3f1dec5fd9120c977645ccb0659f1317995b5
Eric Crudup <briefcasemanx@gmail.com>
2022-03-06 17:37:34 -0800

    adds BreadBaker and PastryBaker methods to Order.cs from external PierresBakery project

--- OrderTracker.Solution/OrderTracker/Models/Order.cs
+++ OrderTracker.Solution/OrderTracker/Models/Order.cs
@@ -6,15 +6,45 @@ namespace OrderTracker.Models
   {
     public string Title {get; set;}
     public string Description { get; set;}
-    public double Price { get; set; }
+
+    public List<Pastry> Pastries = new List<Pastry>{};
+
+    public List<Bread> Breads = new List<Bread>{};
+    public double Price { get; }
     public string Date { get; }
 
-    public Order(string title, string descrip, double price, string date)
+    public Order(string title, string descrip, int pastryNum, int breadNum, string date)
     {
       Title = title;
       Description = descrip;
-      Price = price;
+      Pastries = PastryBaker(pastryNum);
+      Breads = BreadBaker(breadNum);
+      Price = Bread.BreadDiscounter( Breads ) + Pastry.PastryDiscounter( Pastries );
       Date = date;
+    }
+
+    public static List<Bread> BreadBaker( int nBreads)
+    {
+      List<Bread> breadList = new List<Bread>();
+
+      for( int i = 0; i < nBreads; i++)
+      {
+        breadList.Add(new Bread(5));
+      }
+
+      return breadList;
+    }
+
+    public static List<Pastry> PastryBaker( int nPastries)
+    {
+      List<Pastry> PastryList = new List<Pastry>();
+
+      for( int i = 0; i < nPastries; i++)
+      {
+        PastryList.Add(new Pastry(2));
+      }
+
+      return PastryList;
     }
   }
 }
