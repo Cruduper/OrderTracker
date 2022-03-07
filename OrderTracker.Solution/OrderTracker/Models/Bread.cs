ee19aa1e2c10c0714fc3eff73d0f56a7bfb06d7e
Eric Crudup <briefcasemanx@gmail.com>
2022-03-06 17:00:28 -0800

    adds Pastry and Bread classes from external PierresBakery project

---
+++ OrderTracker.Solution/OrderTracker/Models/Bread.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace OrderTracker.Models
+{
+  public class Bread
+  {
+    public int Price{ get; set; }
+
+    public Bread (){ } //overloaded Bread constructor
+    public Bread (int price)
+    {
+      Price = price;
+    }
+
+    public static int BreadDiscounter( List<Bread> loafMountain)
+    {
+      int breadInstance = 1;
+      int totalPrice = 0;
+      foreach (Bread loaf in loafMountain)
+      {
+        if( breadInstance % 3 == 0 )
+          loaf.Price = 0;
+
+        totalPrice += loaf.Price;
+        breadInstance++;
+      }
+      return totalPrice;
+    }
+  }
+}
---
+++ OrderTracker.Solution/OrderTracker/Models/Pastry.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+
+
+namespace OrderTracker.Models
+{
+  public class Pastry
+  {
+    public int Price{ get; set; }
+
+    public Pastry( int price )
+    {
+      Price = price;
+    }
+
+    public Pastry() { } //overloaded Pastry constructor
+
+    public static int PastryDiscounter( List<Pastry> pastryMountain )
+    {
+      int totalPrice = 0;
+      int pastryInstance = 1;
+
+      foreach (Pastry paste in pastryMountain)
+      {
+        if( (pastryInstance % 3 == 0) || (pastryInstance % 6 == 0) )
+          paste.Price = 1;
+
+        totalPrice += paste.Price;
+        pastryInstance++;
+      }
+      
+      return totalPrice;
+    }
+  }
+}
